8b39ff48d7c89dbaa3aee5a229454411633204c8
Lukáš Mačuda <lukas.macuda@gmail.com>
2018-06-11 08:27:45 +0200

    basic functions

---
+++ __tests__/test.js
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+import SmartError from 'smart-error';
+
+import HttpError from '../src';
+
+describe('HttpError', () => {
+
+    it('checks the default values of the HttpError', () => {
+        const error = new HttpError(400);
+        expect(error).to.be.instanceOf(Error);
+        expect(error).to.be.instanceOf(SmartError);
+        expect(error).to.have.all.keys(['message', 'code', 'statusCode']);
+        expect(error.message).to.be.equal('Unknown error');
+        expect(error.code).to.be.equal('ERR_UNKNOWN');
+        expect(error.statusCode).to.be.equal(400);
+    });
+
+    it('checks the set valudes of the HttpError', () => {
+        const error = new HttpError(400, 'Some error message', 'code', { field: 'some_field' });
+        expect(error).to.be.instanceOf(Error);
+        expect(error).to.be.instanceOf(SmartError);
+        expect(error).to.have.all.keys(['message', 'code', 'statusCode', 'field']);
+        expect(error.message).to.be.equal('Some error message');
+        expect(error.code).to.be.equal('ERR_CODE');
+        expect(error.statusCode).to.be.equal(400);
+        expect(error.field).to.be.equal('some_field');
+    });
+
+    it('gets the error instance by the statusCode', () => {
+        const error = HttpError.create(400);
+        expect(error).to.be.instanceOf(Error);
+        expect(error).to.be.instanceOf(SmartError);
+        expect(error).to.have.all.keys(['message', 'code', 'statusCode']);
+        expect(error.message).to.be.equal('Bad Request');
+        expect(error.code).to.be.equal('ERR_BAD_REQUEST');
+        expect(error.statusCode).to.be.equal(400);
+    });
+
+    it('creates the error from the SmartError instance', () => {
+        const error = HttpError.create(400, new SmartError('Some error message', 'code', { field: 'some_field' }));     
+        expect(error).to.be.instanceOf(Error);
+        expect(error).to.be.instanceOf(SmartError);
+        expect(error).to.have.all.keys(['message', 'code', 'statusCode', 'field']);
+        expect(error.message).to.be.equal('Some error message');
+        expect(error.code).to.be.equal('ERR_CODE');
+        expect(error.statusCode).to.be.equal(400);
+        expect(error.field).to.be.equal('some_field');
+    });
+});
